33324f7d65cb71cb52dcac4e1848fb92f688cdb1
josstein09 <U202314290@upc.edu.pe>
2026-07-04 21:26:36 -0500

    pequeño cambio de routerLink

--- src/app/components/usuario/usuario-insertar/usuario-insertar.ts
+++ src/app/components/usuario/usuario-insertar/usuario-insertar.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
@@ -92,3 +93,4 @@ export class UsuarioInsertar implements OnInit {
     }
   }
 }
+
