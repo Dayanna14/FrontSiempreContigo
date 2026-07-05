b2be6981a1bbd4cd1bd432445de0369a783c2079
josstein09 <U202314290@upc.edu.pe>
2026-07-05 15:30:41 -0500

    Funcionamiento de login, home y menu correcto

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { LoginService } from '../../services/login-service';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    RouterLink, RouterModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  constructor(private loginService: LoginService,
+  private router: Router
+  ) {}
+
+  get isLoggedIn(): boolean {
+    return this.loginService.verificar();
+  }
+
+  get userRole(): string | null {
+    return this.loginService.showRole();
+  }
+
+  logout(): void {
+    this.loginService.cerrarSesion();
+    this.router.navigate(['/login']); 
+  }
+}
--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -3,7 +3,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
@@ -23,27 +23,36 @@ import { LoginService } from '../../services/login-service';
     MatMenuModule,
     MatDividerModule,
     RouterLink,
-    RouterOutlet
   ],
   templateUrl: './menu.html',
   styleUrls: ['./menu.css'],
 })
 export class Menucomponent {
+  role: string = '';
+  usuario: string = '';
   constructor(
     private router: Router,
     private loginService: LoginService 
   ) {}
 
-  get isLoggedIn(): boolean {
-    return this.loginService.verificar();
-  }
-
-  get userRole(): string | null {
-    return this.loginService.showRole();
+  verificar(): boolean {
+    const existe = this.loginService.verificar();
+    if (existe) {
+      this.role = this.loginService.showRole() ?? '';
+    }
+    return existe;
   }
 
   logout(): void {
     this.loginService.cerrarSesion();
     this.router.navigate(['/login']); 
+  }
+
+  isPROFESIONAL() {
+    return this.role === 'PROFESIONAL';
+  }
+
+  isPACIENTE() {
+    return this.role === 'PACIENTE';
   }
 }
